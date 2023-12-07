9b7a6f6c455fb79a3f065d2d7d3285ae9e29df2e
salidamaharjan <salidamaharjan@outlook.com>
2023-12-07 15:27:23 -0600

    adding eventListener to button and adding the result to geo.jason and weather.jason file

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 //locating a button from html file to variable stored in DOM
 var searchBtn = document.querySelector(".search-btn");
+
+//adding event listener to button
+searchBtn.addEventListener("click", function() {
+
+})
