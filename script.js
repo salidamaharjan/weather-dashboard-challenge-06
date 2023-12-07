3e5261077863d86e457695f6013e825bc03524e3
salidamaharjan <salidamaharjan@outlook.com>
2023-12-07 17:01:34 -0600

    update function name

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ searchBtn.addEventListener("click", function() {
 fetchWeatherApi();
 })
 function fetchWeatherApi(){
-    var fetchWeatherURL = "./geo.json"
-    fetch(fetchWeatherURL)
+    var fetchGeoURL = "./geo.json"
+    fetch(fetchGeoURL)
     .then(function(response) {
         return response.json();
     }).then(function(data){
         // console.log(data);
-       lonAndLat(data);
+       fetchWeatherWithLatLon(data);
     })
 }
-function lonAndLat(data){
+function fetchWeatherWithLatLon(data){
     for(var i = 0; i < data.length; i++) {
         var country = data[i].country;
         if(country === "US"){
