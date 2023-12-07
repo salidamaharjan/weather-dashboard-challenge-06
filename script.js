baa02ca7a6b984e17c40ff8b2c83828b35b0c475
salidamaharjan <salidamaharjan@outlook.com>
2023-12-07 16:55:53 -0600

    creating function to fetch weahter API

--- script.js
+++ script.js
@@ -3,5 +3,26 @@ var searchBtn = document.querySelector(".search-btn");
 
 //adding event listener to button
 searchBtn.addEventListener("click", function() {
-
+fetchWeatherApi();
 })
+function fetchWeatherApi(){
+    var fetchWeatherURL = "./geo.json"
+    fetch(fetchWeatherURL)
+    .then(function(response) {
+        return response.json();
+    }).then(function(data){
+        // console.log(data);
+       lonAndLat(data);
+    })
+}
+function lonAndLat(data){
+    for(var i = 0; i < data.length; i++) {
+        var country = data[i].country;
+        if(country === "US"){
+            // console.log(country);
+            var lon = data[i].lon;
+            var lat = data[i].lat;
+            // console.log(lon, lat);
+        } 
+    }
+}
