39de22fe2c34ff29b459b71483553d3aa621af3d
salidamaharjan <salidamaharjan@outlook.com>
2023-12-07 22:04:55 -0600

    reviewed code

--- script.js
+++ script.js
@@ -2,27 +2,37 @@
 var searchBtn = document.querySelector(".search-btn");
 
 //adding event listener to button
-searchBtn.addEventListener("click", function() {
-fetchWeatherApi();
-})
-function fetchWeatherApi(){
-    var fetchGeoURL = "./geo.json"
-    fetch(fetchGeoURL)
-    .then(function(response) {
-        return response.json();
-    }).then(function(data){
-        // console.log(data);
-       fetchWeatherWithLatLon(data);
+searchBtn.addEventListener("click", function () {
+  fetchWeatherApi();
+});
+function fetchWeatherApi() {
+  var fetchGeoURL = `./geo.json`;
+  fetch(fetchGeoURL)
+    .then(function (response) {
+      return response.json();
     })
+    .then(function (data) {
+      var latAndLon = fetchWeatherWithLatLon(data);
+      var lat = latAndLon.lat;
+      var lon = latAndLon.lon;
+      var fetchWeatherURL = "./weather.json";
+      fetch(fetchWeatherURL)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
+    });
 }
-function fetchWeatherWithLatLon(data){
-    for(var i = 0; i < data.length; i++) {
-        var country = data[i].country;
-        if(country === "US"){
-            // console.log(country);
-            var lon = data[i].lon;
-            var lat = data[i].lat;
-            // console.log(lon, lat);
-        } 
+function fetchWeatherWithLatLon(geoInfo) {
+  var latAndLon = {};
+  for (var i = 0; i < geoInfo.length; i++) {
+    var country = geoInfo[i].country;
+    if (country === "US") {
+      // console.log(country);
+      return (latAndLon = { lon: geoInfo[i].lon, lat: geoInfo[i].lat });
+      // console.log(lon, lat);
     }
+  }
 }
