d754b77d1ce829a6333df6130080fe99f6896123
salidamaharjan <salidamaharjan@outlook.com>
2023-12-09 09:36:25 -0600

    a function created to display the values in the UI

--- script.js
+++ script.js
@@ -21,18 +21,39 @@ function fetchWeatherApi() {
           return response.json();
         })
         .then(function (data) {
-          console.log(data);
+          displayWeatherData(data)
         });
     });
 }
 function fetchWeatherWithLatLon(geoInfo) {
-  var latAndLon = {};
   for (var i = 0; i < geoInfo.length; i++) {
     var country = geoInfo[i].country;
     if (country === "US") {
       // console.log(country);
-      return (latAndLon = { lon: geoInfo[i].lon, lat: geoInfo[i].lat });
+      return { lon: geoInfo[i].lon, lat: geoInfo[i].lat };
       // console.log(lon, lat);
     }
   }
+}
+function displayWeatherData(data){
+  console.log(data.list[0]);
+  console.log(data.list[0].main.temp);
+  console.log(data.list[0].main.humidity);
+  console.log(data.list[0].wind.speed);
+  tempWindHumidity(".day-1",data.list[0]);
+  tempWindHumidity(".day-2",data.list[0]);
+  tempWindHumidity(".day-3",data.list[0]);
+  tempWindHumidity(".day-4",data.list[0]);
+  tempWindHumidity(".day-5",data.list[0]);
+  tempWindHumidity(".day-6",data.list[0]);
+}
+function tempWindHumidity(day,data){
+  var day1Temp = document.querySelector(`${day} .temp span`);
+  var day1Wind = document.querySelector(`${day} .wind span`);
+  var day1Humidity = document.querySelector(`${day} .humidity span`);
+  
+  day1Temp.textContent = data.main.temp;
+  day1Wind.textContent = data.wind.speed;
+  day1Humidity.textContent = data.main.humidity;
+  
 }
